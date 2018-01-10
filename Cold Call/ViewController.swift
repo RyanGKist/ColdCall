baec15e6cbac08360082b6a395c855f54d0f8d2d
Ryan Kistner <RyanGKistner@Patricks-MacBook-Pro-2.local>
2018-01-10 13:15:21 -0800

    Finished Cold Call v1

--- Cold Call/ViewController.swift
+++ Cold Call/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC_1.swift
 //  Cold Call
 //
 //  Created by Ryan Kistner on 1/10/18.
@@ -8,18 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC_1: UIViewController {
 
+    @IBOutlet weak var personsName: UILabel!
+    
+    let name : [String] = ["Ryan", "Shane", "Pat", "Dan", "Chris", "Karesa"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        personsName.isHidden = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    @IBAction func callButtonPressed(_ sender: Any) {
+        print("Button Pressed")
+        personsName.text = name[Int(arc4random_uniform(UInt32(name.count)))]
+        personsName.isHidden = false
+    }
+    
 }
 
